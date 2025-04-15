21328eb69dc2edc754c308ea940d79a1ac7c9bbe
jonas mendes <jonas.jsm2903@gmail.com>
2025-04-15 17:53:33 -0300

    environments.4

--- src/test/java/com/JonasSmendes/ApiTodoList/ApiTodoListApplicationTests.java
+++ src/test/java/com/JonasSmendes/ApiTodoList/ApiTodoListApplicationTests.java
@@ -2,8 +2,12 @@ package com.JonasSmendes.ApiTodoList;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
+@ActiveProfiles("test")
 @SpringBootTest
+@TestPropertySource("classpath:application-test.properties")
 class ApiTodoListApplicationTests {
 
 	@Test
